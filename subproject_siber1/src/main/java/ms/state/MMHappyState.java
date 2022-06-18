b95898ad78842ef2bae26e9cf9a99b4d7bf560df
bt <dys456123@163.com>
2022-06-18 15:39:38 +0800

    状态模式

--- subproject_siber1/src/main/java/ms/state/MMHappyState.java
+++ subproject_siber1/src/main/java/ms/state/MMHappyState.java
@@ -1,4 +1,9 @@
 package ms.state;
 
 public class MMHappyState {
+    /**
+     * git remote add origin https://github.com/superbt/zyfront.git
+     * git branch -M main
+     * git push -u origin main
+     */
 }
